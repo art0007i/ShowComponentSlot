cc4b3fa60499e7a020078b37bca320d0117360d4
art0007i <art0007i@gmail.com>
2021-09-11 03:57:48 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,8 @@
+# ShowComponentSlot
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that does x.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [ShowComponentSlot.dll](https://github.com/art0007i/ShowComponentSlot/releases/latest/download/ShowComponentSlot.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
---
+++ ShowComponentSlot/ShowComponentSlot.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using FrooxEngine.UIX;
+using BaseX;
+
+namespace ShowComponentSlot
+{
+    public class ShowComponentSlot : NeosMod
+    {
+        public override string Name => "ShowComponentSlot";
+        public override string Author => "art0007i";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/art0007i/ShowComponentSlot/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("me.art0007i.ShowComponentSlot");
+            harmony.PatchAll();
+
+        }
+        [HarmonyPatch(typeof(WorkerInspector))]
+        [HarmonyPatch("BuildUIForComponent")]
+        class WorkerInspector_BuildUIForComponent_Patch
+        {
+            public static void Postfix(Worker worker, bool allowRemove, bool allowDuplicate, WorkerInspector __instance)
+            {
+                if (allowRemove && !allowDuplicate)
+                {
+                    if (!__instance.World.IsUserspace() && !Settings.ReadValue<bool>("ArtiAdditions.EnablePatch", true)) return;
+                    UIBuilder ui = new UIBuilder(__instance.Slot[0][0]);
+                    ui.Style.MinHeight = 24f;
+                    ui.Style.FlexibleWidth = 0f;
+                    ui.Style.MinWidth = 32f;
+
+                    color color = color.Yellow;
+                    color white = color.White;
+                    color color2 = MathX.Lerp(color, white, 0.7f);
+                    var button = ui.Button("⤴", color2);
+                    var edit = button.Slot[0].AttachComponent<RefEditor>();
+                    (AccessTools.Field(edit.GetType(), "_targetRef").GetValue(edit) as RelayRef<ISyncRef>).Target = (ISyncRef)AccessTools.Field(__instance.GetType(), "_targetWorker").GetValue(__instance);
+                    button.Pressed.Target = (ButtonEventHandler)AccessTools.Method(edit.GetType(), "OpenInspectorButton").CreateDelegate(typeof(ButtonEventHandler), edit);
+                }
+            }
+        }
+    }
+}
