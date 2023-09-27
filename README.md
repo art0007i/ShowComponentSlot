981f870e322bd514c49d2ccb899c581115991fe9
art0007i <art0007i@gmail.com>
2023-09-28 00:23:58 +0200

    port to resonite

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # ShowComponentSlot
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allows you to open the slot that a component is attached to in component inspectors.
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that allows you to open the slot that a component is attached to in component inspectors.
 
 ## Installation
-1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-1. Place [ShowComponentSlot.dll](https://github.com/art0007i/ShowComponentSlot/releases/latest/download/ShowComponentSlot.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
-1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Place [ShowComponentSlot.dll](https://github.com/art0007i/ShowComponentSlot/releases/latest/download/ShowComponentSlot.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
--- ShowComponentSlot/ShowComponentSlot.cs
+++ ShowComponentSlot/ShowComponentSlot.cs
@@ -1,22 +1,17 @@
 ﻿using HarmonyLib;
-using NeosModLoader;
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+using ResoniteModLoader;
 using FrooxEngine;
-using FrooxEngine.LogiX;
 using FrooxEngine.UIX;
-using BaseX;
+using Elements.Core;
+using System;
 
 namespace ShowComponentSlot
 {
-    public class ShowComponentSlot : NeosMod
+    public class ShowComponentSlot : ResoniteMod
     {
         public override string Name => "ShowComponentSlot";
         public override string Author => "art0007i";
-        public override string Version => "1.0.0";
+        public override string Version => "2.0.0";
         public override string Link => "https://github.com/art0007i/ShowComponentSlot/";
         public override void OnEngineInit()
         {
@@ -33,14 +28,15 @@ namespace ShowComponentSlot
                 if (allowRemove && !allowDuplicate)
                 {
                     UIBuilder ui = new UIBuilder(__instance.Slot[0][0]);
+                    RadiantUI_Constants.SetupEditorStyle(ui);
                     ui.Style.MinHeight = 24f;
                     ui.Style.FlexibleWidth = 0f;
-                    ui.Style.MinWidth = 32f;
+                    ui.Style.MinWidth = 40f;
 
-                    color color = color.Yellow;
-                    color white = color.White;
-                    color color2 = MathX.Lerp(color, white, 0.7f);
-                    var button = ui.Button("⤴", color2);
+                    var color = colorX.Yellow;
+                    var b = colorX.Black;
+                    var color2 = MathX.Lerp(color, b, 0.7f);
+                    var button = ui.Button(OfficialAssets.Graphics.Icons.Inspector.RootUp, color2);
                     var edit = button.Slot[0].AttachComponent<RefEditor>();
                     (AccessTools.Field(edit.GetType(), "_targetRef").GetValue(edit) as RelayRef<ISyncRef>).Target = (ISyncRef)AccessTools.Field(__instance.GetType(), "_targetWorker").GetValue(__instance);
                     button.Pressed.Target = (ButtonEventHandler)AccessTools.Method(edit.GetType(), "OpenInspectorButton").CreateDelegate(typeof(ButtonEventHandler), edit);
